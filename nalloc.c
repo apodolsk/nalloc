6eac4c2ae810b15ed8e6830c017c366fd0925281
Alex V. Podolsky <podolsky.sasha@gmail.com>
2016-07-01 18:13:22 -0400

    Hid T->nallocin updates behind if_dbg.

--- nalloc.c
+++ nalloc.c
@@ -234,7 +234,8 @@ void (slab_ref_down)(slab *s){
 err (linref_up)(const volatile void *l, type *t){
     assert(l);
     if(t->has_special_ref(l, true))
-        return T->nallocin.linrefs_held++, 0;
+        return if_dbg(T->nallocin.linrefs_held++),
+               0;
     if(l < heap_start() || l > heap_end())
         return EARG();
     
@@ -245,12 +246,13 @@ err (linref_up)(const volatile void *l, type *t){
         log(LINREF_VERB, "linref up! % % %", l, t, tx.linrefs);
         assert(tx.linrefs > 0);
         if(cas2_won(((tyx){t, tx.linrefs + 1}), &s->tx, &tx))
-            return T->nallocin.linrefs_held++, 0;
+            return if_dbg(T->nallocin.linrefs_held++),
+                   0;
     }
 }
 
 void (linref_down)(const volatile void *l, type *t){
-    T->nallocin.linrefs_held--;
+    assert(T->nallocin.linrefs_held--);
     if(!t->has_special_ref(l, false))
         slab_ref_down(slab_of((void *) l));
 }
@@ -356,12 +358,12 @@ void nalloc_profile_report(void){
 #endif  /* NONALLOC */
 
 err fake_linref_up(void){
-    T->nallocin.linrefs_held++;
+    if_dbg(T->nallocin.linrefs_held++);
     return 0;
 }
 
 void fake_linref_down(void){
-    T->nallocin.linrefs_held--;
+    assert(T->nallocin.linrefs_held--);
 }
 
 void linref_account_open(linref_account *a){
